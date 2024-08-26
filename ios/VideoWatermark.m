af14be3bdd877aa0bb2924a0a44516719396808c
Manuel López <92613951+Mannelpz25@users.noreply.github.com>
2024-08-26 17:17:26 -0600

    fix: Add video rotation transformations

--- ios/VideoWatermark.m
+++ ios/VideoWatermark.m
@@ -12,7 +12,6 @@ RCT_EXPORT_METHOD(convert:(NSString *)videoUri imageUri:(nonnull NSString *)imag
 
 -(void)watermarkVideoWithImage:(NSString *)videoUri imageUri:(NSString *)imageUri markerScale:(CGFloat)markerScale callback:(RCTResponseSenderBlock)callback
 {
-    
     AVURLAsset* videoAsset = [[AVURLAsset alloc]initWithURL:[NSURL fileURLWithPath:videoUri] options:nil];
     AVMutableComposition* mixComposition = [AVMutableComposition composition];
     
@@ -20,92 +19,92 @@ RCT_EXPORT_METHOD(convert:(NSString *)videoUri imageUri:(nonnull NSString *)imag
     AVAssetTrack *clipVideoTrack = [[videoAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0];
     AVMutableCompositionTrack *compositionAudioTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
     AVAssetTrack *clipAudioTrack = [[videoAsset tracksWithMediaType:AVMediaTypeAudio] objectAtIndex:0];
-    //If you need audio as well add the Asset Track for audio here
     
     [compositionVideoTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, videoAsset.duration) ofTrack:clipVideoTrack atTime:kCMTimeZero error:nil];
     [compositionAudioTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, videoAsset.duration) ofTrack:clipAudioTrack atTime:kCMTimeZero error:nil];
     
-    [compositionVideoTrack setPreferredTransform:[[[videoAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0] preferredTransform]];
-        
     CGSize sizeOfVideo = CGSizeApplyAffineTransform(clipVideoTrack.naturalSize, clipVideoTrack.preferredTransform);
     sizeOfVideo.width = fabs(sizeOfVideo.width);
+    sizeOfVideo.height = fabs(sizeOfVideo.height);
     
-    //Image of watermark
+    // Adjust the orientation of the watermark image
     UIImage *myImage=[UIImage imageWithContentsOfFile:imageUri];
-    
-    UIGraphicsBeginImageContext(sizeOfVideo);
-    [myImage drawInRect:CGRectMake(0, 0, sizeOfVideo.width, sizeOfVideo.height)];
-    UIImage *destImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    myImage = destImage;
-    
     
     CALayer *layerCa = [CALayer layer];
     layerCa.contents = (id)myImage.CGImage;
-    layerCa.frame = CGRectMake(0, 0, sizeOfVideo.width, sizeOfVideo.height);
     layerCa.opacity = 1.0;
     
-    CALayer *parentLayer=[CALayer layer];
-    CALayer *videoLayer=[CALayer layer];
-    parentLayer.frame=CGRectMake(0, 0, sizeOfVideo.width, sizeOfVideo.height);
-    videoLayer.frame=CGRectMake(0, 0, sizeOfVideo.width, sizeOfVideo.height);
+    CALayer *parentLayer = [CALayer layer];
+    CALayer *videoLayer = [CALayer layer];
     [parentLayer addSublayer:videoLayer];
     [parentLayer addSublayer:layerCa];
     
-    AVMutableVideoComposition *videoComposition=[AVMutableVideoComposition videoComposition] ;
-    videoComposition.frameDuration=CMTimeMake(1, 30);
-    videoComposition.renderSize=sizeOfVideo;
-    videoComposition.animationTool=[AVVideoCompositionCoreAnimationTool videoCompositionCoreAnimationToolWithPostProcessingAsVideoLayer:videoLayer inLayer:parentLayer];
+    layerCa.frame = CGRectMake(0, 0, sizeOfVideo.width, sizeOfVideo.height);
+    parentLayer.frame = CGRectMake(0, 0, sizeOfVideo.width, sizeOfVideo.height);
+    videoLayer.frame = CGRectMake(0, 0, sizeOfVideo.width, sizeOfVideo.height);
     
+    // Apply transformations directly to the video
+    CGAffineTransform videoTransform = CGAffineTransformIdentity;
+    CGAffineTransform trackTransform = clipVideoTrack.preferredTransform;
+
+    // Identify the video orientation and apply the appropriate rotation
+    if (trackTransform.a == 0 && trackTransform.b == -1.0 && trackTransform.c == 1.0 && trackTransform.d == 0) {
+        // 90 degrees clockwise rotation
+        videoTransform = CGAffineTransformMakeRotation(-M_PI_2);
+        videoTransform = CGAffineTransformTranslate(videoTransform, -sizeOfVideo.height, 0);
+    } else if (trackTransform.a == -1.0 && trackTransform.b == 0 && trackTransform.c == 0 && trackTransform.d == -1.0) {
+        // 180 degrees rotation
+        videoTransform = CGAffineTransformMakeRotation(M_PI);
+        videoTransform = CGAffineTransformTranslate(videoTransform, -sizeOfVideo.width, -sizeOfVideo.height);
+    } else if (trackTransform.a == 0 && trackTransform.b == 1.0 && trackTransform.c == -1.0 && trackTransform.d == 0) {
+        // 90 degrees counterclockwise rotation
+        videoTransform = CGAffineTransformMakeRotation(M_PI_2);
+        videoTransform = CGAffineTransformTranslate(videoTransform, 0, -sizeOfVideo.width);
+    }
+
+    // Create the instruction to apply the transformation to the video
     AVMutableVideoCompositionInstruction *instruction = [AVMutableVideoCompositionInstruction videoCompositionInstruction];
     instruction.timeRange = CMTimeRangeMake(kCMTimeZero, [mixComposition duration]);
-    AVAssetTrack *videoTrack = [[mixComposition tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0];
+
+    AVMutableVideoCompositionLayerInstruction *layerInstruction = [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:compositionVideoTrack];
+    [layerInstruction setTransform:videoTransform atTime:kCMTimeZero];
+    instruction.layerInstructions = @[layerInstruction];
     
-    AVMutableVideoCompositionLayerInstruction* layerInstruction = [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:videoTrack];
+    AVMutableVideoComposition *videoComposition = [AVMutableVideoComposition videoComposition];
+    videoComposition.instructions = @[instruction];
+    videoComposition.frameDuration = CMTimeMake(1, 30);
     
-    // https://stackoverflow.com/questions/44911802/video-rotated-after-applying-avvideocomposition/45058026
-    BOOL  isAssetPortrait_  = NO;
-    CGAffineTransform trackTransform = clipVideoTrack.preferredTransform;
-    if(trackTransform.a == 0 && trackTransform.b == 1.0 && trackTransform.c == -1.0 && trackTransform.d == 0)  {
-        isAssetPortrait_ = YES;
-    }
-    if(trackTransform.a == 0 && trackTransform.b == -1.0 && trackTransform.c == 1.0 && trackTransform.d == 0)  {
-        isAssetPortrait_ = YES;
-    }
-    if(isAssetPortrait_){
-        CGAffineTransform assetScaleFactor = CGAffineTransformMakeScale(1.0, 1.0);
-        [layerInstruction setTransform:CGAffineTransformConcat(clipVideoTrack.preferredTransform, assetScaleFactor) atTime:kCMTimeZero];
-    }
-    
-    
-    instruction.layerInstructions = [NSArray arrayWithObject:layerInstruction];
-    videoComposition.instructions = [NSArray arrayWithObject: instruction];
-    
-    NSString *documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)objectAtIndex:0];
+    videoComposition.animationTool = [AVVideoCompositionCoreAnimationTool videoCompositionCoreAnimationToolWithPostProcessingAsVideoLayer:videoLayer inLayer:parentLayer];
+    videoComposition.renderSize = sizeOfVideo;
+
+    // Export the video with the applied rotation
+    NSString *documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"yyyy-MM-dd_HH-mm-ss"];
-    NSString *destinationPath = [documentsDirectory stringByAppendingFormat:@"/output_%@.mov", [dateFormatter stringFromDate:[NSDate date]]];
+    NSString *destinationPath = [documentsDirectory stringByAppendingFormat:@"/output_%@.mp4", [dateFormatter stringFromDate:[NSDate date]]];
     
     AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:mixComposition presetName:AVAssetExportPresetHighestQuality];
-    exportSession.videoComposition=videoComposition;
-    
+    exportSession.videoComposition = videoComposition;
     exportSession.outputURL = [NSURL fileURLWithPath:destinationPath];
-    exportSession.outputFileType = AVFileTypeQuickTimeMovie;
+    exportSession.outputFileType = AVFileTypeMPEG4;
+    
     [exportSession exportAsynchronouslyWithCompletionHandler:^{
-        switch (exportSession.status)
-        {
+        switch (exportSession.status) {
             case AVAssetExportSessionStatusCompleted:
                 NSLog(@"Export OK");
                 callback(@[destinationPath]);
                 break;
             case AVAssetExportSessionStatusFailed:
-                NSLog (@"AVAssetExportSessionStatusFailed: %@", exportSession.error);
+                NSLog(@"AVAssetExportSessionStatusFailed: %@", exportSession.error);
                 break;
             case AVAssetExportSessionStatusCancelled:
                 NSLog(@"Export Cancelled");
                 break;
+            default:
+                break;
         }
     }];
 }
+
 
 @end
